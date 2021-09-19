84f8cb021f4a97c8b69c4c4dc7ae909eddb708ca
Cory Larro <cory.larro@gmail.com>
2021-09-18 17:57:00 -0700

    Added Stymied Project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,13 +16,24 @@ function Projects(props) {
             <Row>
                 <Col md={4}>
                     <Card className="card-project">
-                        <CardImg top width="100%" src="/images/nucamp2.png" alt="Nucampsite image" />
+                        <CardImg top width="100%" src="/images/nucamp2.png" alt="Nucampsite website image" />
                         <br />
                         <CardBody className="cardbody-project">
                         <CardTitle tag="h5" className="text-white text-center sora">Nucampsite</CardTitle>
-                        <CardText className="text-white text-center sora">Nucampsite is a website built collaboratively with other students in my cohort. Utilizing React, Redux and CSS.</CardText>
+                        <CardText className="text-white text-center sora">Nucampsite is a website built collaboratively with other students in my cohort. Utilizing React, Redux and CSS. This project was built during the React portion of the Nucamp Fullstack Mobile and Web Development bootcamp. </CardText>
                         <a href="https://github.com/corylarro/nucampsite2"><i className="fa fa-github text-white fa-lg fa-fw" /></a>{' '}
                         <a href="https://nucamp-campsite.herokuapp.com/home"><i className="fas fa-globe text-white fa-lg fa-fw" /></a>{' '}
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md={4}>
+                    <Card className="card-project">
+                        <CardImg top width="100%" src="/images/stymied.png" alt="Stymied Golf website image" />
+                        <CardBody className="cardbody-project">
+                        <CardTitle tag="h5" className="text-white text-center sora pb-2">Stymied Golf</CardTitle>
+                        <CardText className="text-white text-center sora pb-4">Stymied Golf is a solo project built using HTML, Bootstrap and CSS. The project was build during the front-end Bootstrap portion of the Nucamp Fullstack Mobile and Web Development bootcamp.</CardText>
+                        <a href="https://github.com/corylarro/stymiedgolf"><i className="fa fa-github text-white fa-lg fa-fw" /></a>{' '}
+                        <a href="https://peaceful-bose-8d7e14.netlify.app/index.html"><i className="fas fa-globe text-white fa-lg fa-fw" /></a>{' '}
                         </CardBody>
                     </Card>
                 </Col>
